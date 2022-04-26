e06adc72ca4951fc7ffc19710e6ae408cf64395b
Svaho <svahoofficial@gmail.com>
2022-04-26 22:58:04 +0200

    rps

---
+++ src/components/Picks.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Pick from "./Pick";
+
+export default function Picks(props) {
+  const { playerPick, computerPick } = props;
+  return (
+    <div className="picks">
+      <Pick name={playerPick} description="You picked" />
+      <Pick
+        name={computerPick}
+        description="The house picked"
+        isLoadingDot={true}
+      />
+    </div>
+  );
+}
---
+++ src/containers/Phases.js
@@ -0,0 +1,36 @@
+import React from "react";
+import FirstPhase from "./FirstPhase";
+import SecondPhase from "./SecondPhase";
+import { connect } from "react-redux";
+import { PHASE_1, PHASE_2 } from "../constants/constants";
+
+const Phases = (props) => {
+  const { displayRulesModal, modalVisible, phase, playerPick, computerPick } =
+    props;
+  return (
+    <>
+      {phase === PHASE_1 && (
+        <FirstPhase
+          modalVisible={modalVisible}
+          displayRulesModal={displayRulesModal}
+        />
+      )}
+      {phase === PHASE_2 && (
+        <SecondPhase
+          modalVisible={modalVisible}
+          displayRulesModal={displayRulesModal}
+          playerPick={playerPick}
+          computerPick={computerPick}
+        />
+      )}
+    </>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  phase: state.phases.currentPhase,
+  playerPick: state.game.playerPick,
+  computerPick: state.game.computerPick,
+});
+
+export default connect(mapStateToProps)(Phases);
